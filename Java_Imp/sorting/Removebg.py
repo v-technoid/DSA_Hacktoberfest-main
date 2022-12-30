e379799942cc38d736d92b304a7aef41da48e996
v-technoid <98532135+v-technoid@users.noreply.github.com>
2022-12-30 14:10:29 +0530

    Update 1

--- Java_Imp/sorting/Removebg.py
+++ Java_Imp/sorting/Removebg.py
@@ -2,6 +2,6 @@ from rembg import remove
 from PIL import Image
 input_path = 'cl.png'
 output_path = 'output.png'
-input = Image.open (input_path)
+input = Image.open(input_path)
 output = remove(input)
 output.save(output_path)
